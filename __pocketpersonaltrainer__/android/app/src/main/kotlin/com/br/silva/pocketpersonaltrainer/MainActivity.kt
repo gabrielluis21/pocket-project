f40eb68868382734f33d2366d95ec2f50a450da3
gabrielluis21 <gabrielluis_21@hotmail.com>
2025-02-18 14:10:18 -0300

    removed unnecessary folders

--- __pocketpersonaltrainer__/android/app/src/main/kotlin/com/br/silva/pocketpersonaltrainer/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.br.silva.pocketpersonaltrainer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- __pocketpersonaltrainer__/lib/src/localization/localizations_extensions.dart
+++
@@ -1,8 +0,0 @@
-
-import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
-
-extension LocalizationsExtensions on BuildContext{
-  AppLocalizations get i18n => AppLocalizations.of(this)!;
-}
